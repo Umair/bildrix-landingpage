55fcdd50294c623c6a5d10c46c660ccd2ac2170f
Umair Ejaz <umairejaz.ch@gmail.com>
2026-02-08 21:52:11 +0500

    vercel deployment

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Bildrix from "../bildrix.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Bildrix />
+  </React.StrictMode>
+);
