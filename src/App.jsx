8a2e8b0ba6f84a60efe5e9bda11a61b88d184f3c
Umair Ejaz <umairejaz.ch@gmail.com>
2026-02-11 19:50:00 +0500

    ana;ytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Bildrix from "../bildrix.jsx";
 import ArticlePage from "./ArticlePage.jsx";
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Bildrix />} />
-            <Route path="/blog/:slug" element={<ArticlePage />} />
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<Bildrix />} />
+                <Route path="/blog/:slug" element={<ArticlePage />} />
+            </Routes>
+            <Analytics />
+        </>
     );
 }
